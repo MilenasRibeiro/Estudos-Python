63cf48d11418d3213bf3d0f0d9cdbf02a849a9b9
MilenasRibeiro <milenadesouzaribeiro@outlook.com>
2022-01-13 21:30:11 -0300

    estudos python

---
+++ 3.1.py
@@ -0,0 +1,8 @@
+#Informar o valor maior
+a = int(input("Primeiro valor :"))
+b = int(input ("Segundo valor :"))
+
+if a > b:
+         print("O maior número é {}" . format (a))
+print ("Final do Programa")
+   
---
+++ 3.2.py
@@ -0,0 +1,10 @@
+#Informar o valor maior
+a = int(input("Primeiro valor :"))
+b = int(input ("Segundo valor :"))
+
+if a > b:
+         print("O maior número é {}" . format (a))
+else: 
+    print ("O maior número é {}". format(b))
+print ("Final do Programa")
+   
